968feea3ccf11d6e2716c8549701098f5846c78d
Aryan Rai <aryan.rai@gmail.com>
2025-07-10 17:44:11 +0530

    layout changes done

---
+++ src/app/Modal/Employee.ts
@@ -0,0 +1,11 @@
+export class Employee{
+    'employeeId':number;
+    'employeeName':string;
+    'contactNo':String;
+    'emilId':String;
+    'deptId':string;
+    'password':string;
+    'gender':String;
+    'role':String;
+    'createdData':String;
+}
